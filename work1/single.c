b1fe773f8ab6642cc4adc2cd4d6e113adac42f59
Sambo Chea <sombochea@cubetiqs.com>
2020-08-02 11:27:05 +0700

    Add function struct and tags for person tag and course tag Add header functions

--- work1/single.c
+++ work1/single.c
@@ -1,8 +1,28 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-int main() {
+struct person_tag {
+    char name[20];
+    char id[10];
+};
+
+struct course_tag {
+    char course_name[20];
+    int no_of_units;
+    int marks[4];
+    float avg;
+};
+
+void display_students(void);
+void search_student(void);
+void find_maximum(void);
+void find_failed(void);
+void update_file();
+void read_file();
+void quite();
+
+int main(void) {
     printf("Welcome!");
-    
+
     printf("Goodbye!");
 }
