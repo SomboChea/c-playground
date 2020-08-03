80a8b001de1143b67247ef865402c2e04ec9e048
Sambo Chea <sombochea@cubetiqs.com>
2020-08-03 13:36:14 +0700

    Init function header

--- work1/q3.c
+++ work1/q3.c
@@ -2,13 +2,26 @@
 #include "stdlib.h"
 #include "string.h"
 
+#define DEPOT_SIZE 10
+
 struct Bus {
     int BusID;
     int RouteID;
     time_t schedule;
-};
+} Depot[DEPOT_SIZE];
+
+void createBuses();
+void printBuses();
+void scheduleBuses();
+void alignupBuses();
+void releaseBuses();
+void emergency();
 
 int main() {
 
     return EXIT_SUCCESS;
+}
+
+void createBuses() {
+    // do something
 }
