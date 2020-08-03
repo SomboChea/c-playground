a290788bd3b8ee0c753bdc032badfbebba210bc4
Sambo Chea <sombochea@cubetiqs.com>
2020-08-03 13:22:48 +0700

    Init q3

---
+++ work1/q3.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+struct Bus {
+    int BusID;
+    int RouteID;
+    time_t schedule;
+};
+
+int main() {
+
+    return EXIT_SUCCESS;
+}
